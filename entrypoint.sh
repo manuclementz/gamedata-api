e6451126db5a529c67c330c12ee3dd374c859a46
Manu Clementz <manu.clementz@gmail.com>
2024-11-28 12:14:11 +0100

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 set -e
 
 echo "Starting Gunicorn server..."
-exec waitress-serve --listen=0.0.0.0:8000 --url-scheme=https gamedata_api:app
+exec waitress-serve --listen=0.0.0.0:8000 --url-scheme=https --trusted-proxy='*' --trusted-proxy-headers='x-forwarded-for,x-forwarded-proto,x-forwarded-host' gamedata_api:app
